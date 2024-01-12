65568e3102c16a77c9e1f3b17b6658201c1b854b
Diana Grzelak <diana-teodora.grzelak@sdworx.com>
2024-01-12 13:46:13 +0100

    add course service

---
+++ StudentManagement.Services/Services/CourseService.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using StudentManagement.Models.Entities;
+using StudentManagment.Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Services.Services
+{
+    public class CourseService
+    {
+        private readonly ICourseRepository _courseRepository;
+        private readonly IMapper _mapper;
+
+        public CourseService(ICourseRepository courseRepository, IMapper mapper)
+        {
+            _courseRepository = courseRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<Course>> GetCoursesAsync()
+        {
+            return await _courseRepository.GetCoursesAsync();
+        }
+
+        public async Task<Course> GetCourseByIdAsync(int courseId)
+        {
+            return await _courseRepository.GetCourseByIdAsync(courseId);
+        }
+
+        public async Task InsertCourseAsync(Course course)
+        {
+            await _courseRepository.InsertCourseAsync(course);
+        }
+
+        public async Task DeleteCourseAsync(int courseId)
+        {
+            await _courseRepository.DeleteCourseAsync(courseId);
+        }
+
+        public async Task UpdateCourseAsync(Course course)
+        { 
+            await _courseRepository.UpdateCourseAsync(course);
+        }
+    }
+}

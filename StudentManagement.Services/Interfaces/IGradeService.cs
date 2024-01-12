ff6e15312ebe350b3697740e3e514aadc14a6c3e
monika-dudzinska <monika.dudzinska3@gmail.com>
2024-01-12 11:37:42 +0100

    create grade service

---
+++ StudentManagement.Services/Interfaces/IGradeService.cs
@@ -0,0 +1,16 @@
+﻿using StudentManagement.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Services.Interfaces
+{
+    public interface IGradeService
+    {
+        Task<IEnumerable<Grade>> GetGradesAsync();
+        Task<Grade> GetGradeByIdAsync(int gradeId);
+        Task InsertGradeAsync(Grade grade);
+        Task DeleteGradeAsync(int gradeId);
+        Task UpdateGradeAsync(Grade grade);
+    }
+}
---
+++ StudentManagement.Services/Services/GradeService.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using StudentManagement.Models.Entities;
+using StudentManagement.Services.Interfaces;
+using StudentManagment.Data.Repositories.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using StudentManagment.Data.Repositories.Interfaces;
+
+namespace StudentManagement.Services.Services
+{
+    public class GradeService : IGradeService
+    {
+        private readonly IGradeRepository _gradeRepository;
+        private readonly IMapper _mapper;
+
+        public GradeService(IGradeRepository gradeRepository, IMapper mapper)
+        {
+            _gradeRepository = gradeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<Grade>> GetGradesAsync()
+        {
+            return await _gradeRepository.GetGradesAsync();
+        }
+
+        public async Task<Grade> GetGradeByIdAsync(int gradeId)
+        {
+            return await _gradeRepository.GetGradeByIdAsync(gradeId);
+        }
+
+        public async Task InsertGradeAsync(Grade grade)
+        {
+            await _gradeRepository.InsertGradeAsync(grade);
+        }
+
+        public async Task DeleteGradeAsync(int gradeId)
+        {
+            await _gradeRepository.DeleteGradeAsync(gradeId);
+        }
+
+        public async Task UpdateGradeAsync(Grade grade)
+        {
+            await _gradeRepository.UpdateGradeAsync(grade);
+        }
+    }
+}
+

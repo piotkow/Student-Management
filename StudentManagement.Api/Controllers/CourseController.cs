273768ccd817f3637185dcc2a940faf964bbf681
piotkow <piotkow133@student.polsl.pl>
2024-01-15 15:57:05 +0100

    add Coaching,Course,Course Training Controllers

---
+++ StudentManagement.Api/Controllers/CourseController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagement.Models.Entities;
+using StudentManagement.Services.Interfaces;
+
+namespace StudentManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Course>>> GetCourses()
+        {
+            var courses = await _courseService.GetCoursesAsync();
+            return Ok(courses);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Course>> GetCourse(int id)
+        {
+            var course = await _courseService.GetCourseByIdAsync(id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(course);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCourse(int id, Course course)
+        {
+            if (id != course.CourseID)
+            {
+                return BadRequest();
+            }
+
+            await _courseService.UpdateCourseAsync(course);
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Course>> PostCourse(Course course)
+        {
+            await _courseService.InsertCourseAsync(course);
+
+            return CreatedAtAction("GetCourse", new { id = course.CourseID }, course);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCourse(int id)
+        {
+            await _courseService.DeleteCourseAsync(id);
+
+            return NoContent();
+        }
+    }
+
+}
